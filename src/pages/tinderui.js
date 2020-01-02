651f65108419ebb188f93a2c88c428c2302da84b
Takahiro <tkhr_621@docomo.ne.jp>
2020-01-03 00:34:11 +0900

    Update tinderui.js

--- src/pages/tinderui.js
+++ src/pages/tinderui.js
@@ -14,25 +14,25 @@ import Img from 'gatsby-image';
         id="about"
       >
           <div className="w-100">
-              <h2 className="mb-5">Tinder風UI</h2>
+              <h2 className="mb-5">UI LIKE MATCHING APP</h2>
               <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
                   <div className="resume-content">
                       <div>
                           <Img fixed={data.file.childImageSharp.fixed} />
                         </div>
                       <ul>
-                        <li>タイトル</li>
-                        <li>開発期間</li>
-                        <li>開発人数</li>
-                        <li>役割</li>
-                        <li>概要</li>
-                        <li>開発言語: React</li>
-                        <li>URL</li>
+                        <li>タイトル: マッチングアプリ風のUI</li>
+                        <li>開発期間: 2週間</li>
+                        <li>開発人数: 1人</li>
+                        <li>役割: プログラミング</li>
+                        <li>概要: マッチングアプリ風のUIをreact-swipeable-cardsを使って作りました。カード下部をタップすると詳細を表示できます。</li>
+                        <li>使用技術: React</li>
+                        <li>URL:  <a href="https://react-matching-card.netlify.com/" rel="noopener noreferrer" target="_blank">https://react-matching-card.netlify.com/</a></li>
                         <li>GitHub: <a href="https://github.com/TakaShinoda/CardUI" rel="noopener noreferrer" target="_blank">https://github.com/TakaShinoda/CardUI</a></li>      
                       </ul>
                     </div>
                 </div>
-                <Link to="./">
+                <Link to="./#works">
                     <p>&lt;&lt; back</p>
                 </Link>
             </div>
